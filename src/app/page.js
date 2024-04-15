ddf08ca44551a34104917dd440bd2988fd5329b8
EsGui <guilhermedagama2021100@gmail.com>
2024-04-14 22:22:59 -0300

    deletando slide

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
 import HomeFooter from './components/home_components/HomeFooter'
 import HomeHeader from './components/home_components/HomeHeader'
 import HomeProducts from './components/home_components/HomeProducts'
-import HomeSlider from './components/home_slider/HomeSIider'
 
 export default function Home() {
   return (
     <main>
       <HomeHeader />
-      <HomeSlider />
       <HomeProducts />
       <HomeFooter />
     </main>
